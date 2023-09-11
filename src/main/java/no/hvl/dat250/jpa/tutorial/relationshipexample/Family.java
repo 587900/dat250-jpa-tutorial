dcb829552aef455f31347363c26786c4d3942940
KjetilBerg <kjetil.a.berg@hotmail.com>
2023-09-11 11:35:33 +0200

    Add Family, Job, Person and tests

---
+++ src/main/java/no/hvl/dat250/jpa/tutorial/relationshipexample/Family.java
@@ -0,0 +1,31 @@
+package no.hvl.dat250.jpa.tutorial.relationshipexample;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import java.util.ArrayList;
+import java.util.List;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+public class Family {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String description;
+
+    @OneToMany(mappedBy = "family")
+    private final List<Person> members = new ArrayList<>();
+
+    public Long getId() { return id; }
+    public String getDescription() { return description; }
+    public void setDescription(String description) { this.description = description; }
+    public List<Person> getMembers() { return members; }
+}
